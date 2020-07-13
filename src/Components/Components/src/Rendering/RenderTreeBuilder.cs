9e694805450d7474abb8796970bec1b1204e6c6e
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-07-13 14:25:39 +0100

    TEMP: Try streamlining AddAttribute, though doesn't help in the FastGrid case

--- src/Components/Components/src/Rendering/RenderTreeBuilder.cs
+++ src/Components/Components/src/Rendering/RenderTreeBuilder.cs
@@ -220,7 +220,8 @@ namespace Microsoft.AspNetCore.Components.Rendering
             AssertCanAddAttribute();
             if (value != null || _lastNonAttributeFrameType == RenderTreeFrameType.Component)
             {
-                Append(RenderTreeFrame.Attribute(sequence, name, value));
+                _entries.AppendAttribute(sequence, name, value);
+                //Append(RenderTreeFrame.Attribute(sequence, name, value));
             }
             else
             {
